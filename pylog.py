b6615b1b067cb4d21ae56dca3ec80daf7d4fb91c
Jackson Tanner <sheev2004@gmail.com>
2026-06-14 14:09:38 -0700

    Added command-line file argument

---
+++ pylog.py
@@ -0,0 +1,25 @@
+import sys
+
+error_count = 0
+warning_count = 0
+info_count = 0
+
+if len(sys.argv) != 2:
+    print("Usage: python pylog.py <logfile>")
+    sys.exit(1)
+
+filename = sys.argv[1]
+
+with open(filename) as file:
+    for line in file:
+        if "ERROR" in line:
+            error_count += 1
+        elif "WARNING" in line:
+            warning_count += 1
+        elif "INFO" in line:
+            info_count += 1 
+    print("Log Summary")
+    print("-----------")
+    print("ERROR: ", error_count)
+    print("WARNING: ", warning_count)
+    print("INFO: ", info_count)
