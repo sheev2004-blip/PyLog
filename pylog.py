f3b8bc54e429f96a8142cdab62b8128de042a68d
Jackson Tanner <sheev2004@gmail.com>
2026-06-14 14:14:41 -0700

    Handles missing log files

--- pylog.py
+++ pylog.py
@@ -9,17 +9,20 @@ if len(sys.argv) != 2:
     sys.exit(1)
 
 filename = sys.argv[1]
-
-with open(filename) as file:
-    for line in file:
-        if "ERROR" in line:
-            error_count += 1
-        elif "WARNING" in line:
-            warning_count += 1
-        elif "INFO" in line:
-            info_count += 1 
+try:
+    with open(filename) as file:
+        for line in file:
+            if "ERROR" in line:
+                error_count += 1
+            elif "WARNING" in line:
+                warning_count += 1
+            elif "INFO" in line:
+                info_count += 1 
     print("Log Summary")
     print("-----------")
     print("ERROR: ", error_count)
     print("WARNING: ", warning_count)
     print("INFO: ", info_count)
+except FileNotFoundError:
+    print("Error: File not found.")
+    sys.exit(1)
